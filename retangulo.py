8a113ba4871719f4c3873a39dfa9465605b3a58e
jscienciadados <62901759+jscienciadados@users.noreply.github.com>
2024-08-03 15:29:51 -0300

    Add files via upload

---
+++ retangulo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  3 15:19:48 2024
+
+Programa que calcule a área de um triângulo. O programa deve solicitar ao usuário que forneça a base e altura.
+Em seguida, o programa deve calcular a área usando a fórmula: area = base x altura
+
+@author: Joao Souza
+"""
+base = float(input('Digite a base do triangulo: '))
+altura = float(input('Digite a altura do triangulo: '))
+
+area = base * altura
+
+print(f'A Área do Trinângulo Retângulo é: {area:.2f}')
+
+
